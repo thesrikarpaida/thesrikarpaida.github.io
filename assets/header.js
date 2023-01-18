95a17cdf620845b4d44f9d7a467e2acc47100715
Srikar Paida <36519233+thesrikarpaida@users.noreply.github.com>
2023-01-18 17:39:31 +0530

    Warning text on Blog and CV

--- assets/header.js
+++ assets/header.js
@@ -1,3 +1,27 @@
-// const heading = "Srikar Paida";
+const blog = document.querySelector('.header-site a');
+const cv = document.querySelector('.header-site a:last-child');
 
-// document.querySelector('.header-name').textContent = heading;
+blog.addEventListener('mouseover', notYetBlog);
+blog.addEventListener('mouseout', notYetBlog);
+
+cv.addEventListener('mouseover', notYetCV);
+cv.addEventListener('mouseout', notYetCV);
+
+
+function notYetBlog(e) {
+    console.log(e);
+    if (e.type == 'mouseover') {
+        e.srcElement.childNodes[0].data = "Not Yet";
+    }
+    else if (e.type == 'mouseout') {
+        e.srcElement.childNodes[0].data = "Blog";
+    }
+}
+function notYetCV(e) {
+    if (e.type == 'mouseover') {
+        e.srcElement.childNodes[0].data = "Not Yet";
+    }
+    else if (e.type == 'mouseout') {
+        e.srcElement.childNodes[0].data = "CV";
+    }
+}
